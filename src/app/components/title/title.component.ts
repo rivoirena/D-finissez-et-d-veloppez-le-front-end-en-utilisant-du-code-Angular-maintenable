735bf496531df91b14ca621bb68a5888f31b33fd
rivoirena <rivoiren38@gmail.com>
2026-08-10 14:31:15 +0200

    include metric component in title component

--- src/app/components/title/title.component.ts
+++ src/app/components/title/title.component.ts
@@ -1,12 +1,15 @@
 import { Component, Input } from '@angular/core';
+import { Metric } from 'src/app/core/models/olympic.model';
+import { MetricsComponent } from '../metrics/metrics.component';
 
 @Component({
   selector: 'app-title',
   standalone: true,
-  imports: [],
+  imports: [MetricsComponent],
   templateUrl: './title.component.html',
   styleUrl: './title.component.scss'
 })
 export class TitleComponent {
   @Input() titlePage: string = '';
+  @Input() metrics: Metric[] = [];
 }
