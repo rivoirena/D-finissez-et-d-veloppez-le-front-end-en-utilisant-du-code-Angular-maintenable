69b0da5edf02120fb9fcdd6e3dd2bd326a6dc919
rivoirena <rivoiren38@gmail.com>
2026-08-11 17:30:32 +0200

    fix: redirect to not found instead of home when country doesn't exist

--- src/app/pages/country/country.component.ts
+++ src/app/pages/country/country.component.ts
@@ -28,7 +28,7 @@ export class CountryComponent implements OnInit {
         if (data && data.length > 0) {
           const selectedCountry = this.olympicService.getCountry(data, countryName ?? '');
           if (!selectedCountry) {
-            this.router.navigate(['/']);
+            this.router.navigate(['not-found']);
             return;
           }
           this.titlePage = selectedCountry?.country ?? '';
